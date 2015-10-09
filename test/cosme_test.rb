605f1b0df2f9c52abde62bd8133ff7d32f31acee
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-10-09 12:50:26 +0900

    Add test cases for @disable_auto_cosmeticize

--- test/cosme_test.rb
+++ test/cosme_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CosmeTest < ActiveSupport::TestCase
   teardown do
     Cosme.instance_variable_set(:@cosmetics, nil)
+    Cosme.instance_variable_set(:@disable_auto_cosmeticize, nil)
   end
 
   test 'has a version number' do
@@ -25,5 +26,19 @@ class CosmeTest < ActiveSupport::TestCase
     Cosme.define(target: '.example', action: :before)
     cosmetics = Cosme.instance_variable_get(:@cosmetics)
     assert_equal Cosme.all, cosmetics.values
+  end
+
+  test '#disable_auto_cosmeticize! sets @disable_auto_cosmeticize' do
+    Cosme.disable_auto_cosmeticize!
+    assert Cosme.instance_variable_get(:@disable_auto_cosmeticize)
+  end
+
+  test '#auto_cosmeticize? returns false when @disable_auto_cosmeticize is true' do
+    Cosme.disable_auto_cosmeticize!
+    refute Cosme.auto_cosmeticize?
+  end
+
+  test '#auto_cosmeticize? returns true when @disable_auto_cosmeticize is false' do
+    assert Cosme.auto_cosmeticize?
   end
 end
